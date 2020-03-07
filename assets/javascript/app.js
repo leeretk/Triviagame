a1a40688b37a9aee81246d707c276d25f9e4dcf1
Kamy <kamy.leeret@uchealth.org>
2020-03-07 10:43:37 -0700

    button fixes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,10 +5,11 @@
 $(document).ready(function () {
 
     var display = document.getElementById("display");
-    var AnswerOne = document.getElementById("answer-one");
-    var AnswerTwo = document.getElementById("answer-two");
-    var AnswerThree = document.getElementById("answer-three");
-    var AnswerFour = document.getElementById("answer-four");
+    var answerOne = document.getElementById("answer-one");
+    var answerTwo = document.getElementById("answer-two");
+    var answerThree = document.getElementById("answer-three");
+    var answerFour = document.getElementById("answer-four");
+    var mySound;
 
 
     var questionArray = [
@@ -33,8 +34,12 @@ $(document).ready(function () {
             correctAnswer: "Hagrid's Guard Dog"
         },
     ]
-
+    
     function playgame() {
+        // var bleep = new audio();
+        // bleep.src="8d82b5_Harry_Potter_Intro_Sound_Effect.mp3";
+        event.preventDefault();
+       
         timerRunning = true;
         display.innerHTML = "10";
         clearInterval(intervalId);
@@ -45,10 +50,10 @@ $(document).ready(function () {
         console.log("Current Question :" + questionArray[currentQuestion].ques);
 
         //Possible Answers to the Current Question: these are the values that appear in the buttons
-        AnswerOne.innerHTML = questionArray[currentQuestion].choices[0];
-        AnswerTwo.innerHTML = questionArray[currentQuestion].choices[1];
-        AnswerThree.innerHTML = questionArray[currentQuestion].choices[2];
-        AnswerFour.innerHTML = questionArray[currentQuestion].choices[3];
+        answerOne.innerHTML = questionArray[currentQuestion].choices[0];
+        answerTwo.innerHTML = questionArray[currentQuestion].choices[1];
+        answerThree.innerHTML = questionArray[currentQuestion].choices[2];
+        answerFour.innerHTML = questionArray[currentQuestion].choices[3];
 
         console.log("Answer One: " + questionArray[currentQuestion].choices[0]);
         console.log("Answer two: " + questionArray[currentQuestion].choices[1]);
@@ -60,8 +65,8 @@ $(document).ready(function () {
         console.log("Correct Answer: " + questionArray[currentQuestion].correctAnswer);
     };
 
-
     function nextquestion() {
+        event.preventDefault();
         $("#next-question").on("click", questionArray[currentQuestion].ques++);
         console.log("Next Question :" + questionArray[currentQuestion].ques++);
     }
@@ -74,6 +79,7 @@ $(document).ready(function () {
     correctAnswer = document.getElementById("correct-answer");
 
     function recordattempt() {
+        event.preventDefault();
         var converted = timeConverter(time);
         attempt++;
         $("#attempts").append("<p>" + "   Attempt: " + attempt + "  Answer: " + "</p>");
@@ -99,6 +105,7 @@ $(document).ready(function () {
 
     //decrement counter
     function decrement() {
+
         if (timer > 0) {
             timer--;
             $("#show-timer").html("<h1>" + timer + "</h1>");
@@ -117,6 +124,7 @@ $(document).ready(function () {
         display.innerHTML = "0";
         $("#display").text("");
         recordattempt();
+        event.preventDefault();
             }
 
     function timeConverter(t) {
@@ -132,21 +140,30 @@ $(document).ready(function () {
         }
         return minutes + ":" + seconds;
     }
+    $("#display").html("<h3>" + "#display" + "</h23");
+
 
     function checkAnswers() {
 
         if (correctAnswer === AnswerOne) {
-           
-            console.log("You picked the correct answer!" + correctAnswer)
-            correctAnswerCount++;
-            console.log("Correct Count " + correctAnswerCount);
-            console.log(correctAnswerCount);
-              reset();
-        } else {
-            incorrectAnswerCount++;
-            console.log("Incorrect Count " + incorrectAnswerCount);
-            console.log("Incorrect Answer Try Again!");
-            reset();
+               console.log("You picked the correct answer!" + correctAnswer)
+               correctAnswerCount++;
+               console.log("Correct Count " + correctAnswerCount);
+               console.log(correctAnswerCount);
+               reset();
+             } else {
+               incorrectAnswerCount++;
+               console.log("Incorrect Count " + incorrectAnswerCount);
+               console.log("Incorrect Answer Try Again!");
+               reset();
+           }
+
+           function answerQuestion() {
+            $("#answer-question").on("click", checkAnswers());
+            console.log("answer-question" + answerQuestion);
         }
-    }
+        
+
+    };
+  
 });
